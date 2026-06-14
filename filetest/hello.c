ac12321cbbaa583225d7010468e24e47c257542d
mirenshairbub3897-bit <mirenshairbub3897@gmail.com>
2026-06-15 00:31:09 +0530

    Update hello.c

--- filetest/hello.c
+++ filetest/hello.c
@@ -4,18 +4,16 @@
 
 static int __init hello_init(void)
 {
-    printk(KERN_INFO "HELLO: OnePlus kernel module loaded!\n");
+    pr_info("HELLO: module loaded\n");
     return 0;
 }
 
 static void __exit hello_exit(void)
 {
-    printk(KERN_INFO "HELLO: OnePlus kernel module unloaded!\n");
+    pr_info("HELLO: module unloaded\n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("you");
-MODULE_DESCRIPTION("OnePlus SM8650 Hello Kernel Module");
